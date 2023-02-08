6f2cf99021abdb0c9efebb1beaabdf6ad760770d
Pratham Pandey <pratham1.pp@gmail.com>
2023-02-09 01:10:58 +0530

    flush

---
+++ starter.js
@@ -0,0 +1,25 @@
+$(".dice1").hover(function(){
+  $(this).attr("src","images/dice2.png")
+},function(){
+  $(this).attr("src","images/dice1.png")
+});
+$(".title-image1").hover(function(){
+  $(this).attr("src","images/lady.webp");
+},function(){
+  $(this).attr("src","images/cuppic.webp");
+});
+$(".title-image2").hover(function(){
+  $(this).attr("src","images/cuppic2.webp");
+},function(){
+  $(this).attr("src","images/bottlepic.webp");
+});
+$(".title-image3").hover(function(){
+  $(this).attr("src","images/bottlepic.webp");
+},function(){
+  $(this).attr("src","images/cuppic2.webp");
+});
+$(".title-image4").hover(function(){
+  $(this).attr("src","images/cuppic.webp");
+},function(){
+  $(this).attr("src","images/lady.webp");
+});
